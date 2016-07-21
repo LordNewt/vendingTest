f30eb5dc4649ef24a3abd96ed60a4f4b114c3528
Sam Schmit <sschmit@gmail.com>
2016-07-20 22:34:24 -0500

    Tests pass

--- src/main/VendingMachine.groovy
+++ src/main/VendingMachine.groovy
@@ -1,4 +1,36 @@
 package main
 
 class VendingMachine {
+
+    //----------------------//
+    //  Member variables    //
+    //----------------------//
+
+    float balance = 0.00
+
+    Map<String,Float> coins = [
+        "nickel": 0.05,
+        "dime": 0.10,
+        "quarter": 0.25
+    ]
+
+
+    //------------------//
+    //  Public methods  //
+    //------------------//
+
+    String displayBalance() {
+        if (balance > 0) {
+            return String.format("Balance: \$%.2f", balance)
+        }
+        return "INSERT COIN"
+    }
+
+    String insertCoin(String coin) {
+        if (coins.containsKey(coin.toLowerCase())) {
+            balance += coins[coin.toLowerCase()]
+        }
+        displayBalance()
+    }
+
 }
