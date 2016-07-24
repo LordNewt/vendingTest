1d4dcc4041b5cf52244823ff232dadd6a2bf20cc
Sam Schmit <sschmit@gmail.com>
2016-07-24 12:37:32 -0500

    Product selection code

--- src/main/Console.groovy
+++ src/main/Console.groovy
@@ -12,13 +12,20 @@ Welcome to RoboBroVend!
 To insert a coin, type "insert <coin name>" (i.e. insert nickel).
 * Note this machine accepts only nickels, dimes and quarters,
   one at a time.
+
+To purchase an item, type "buy <item>" (i.e. buy soda).
+
+At any time, type "help" for a list of commands.
 --
 '''
+        // On first run, display menu and balance
+        //----------------------------------------
+        machine.displayMenu().each { println it }
+        println "--"
+        println machine.displayBalance()
+        println "--"
+
         while(!leaveMachine) {
-            // Begin by displaying balance
-            //-----------------------------
-            println machine.displayBalance()
-            println "--"
 
             // Prompt user for input
             //-----------------------
@@ -33,11 +40,26 @@ To insert a coin, type "insert <coin name>" (i.e. insert nickel).
                 } else {
                     println "Must supply a coin as well, for example: insert nickle"
                 }
+                machine.displayBalance()
+            } else if (userInput[0] == "buy") {
+                if (userInput.size() > 1) {
+                    println machine.selectProduct(userInput[1])
+                } else {
+                    println "Must supply a product as well, for example: buy soda"
+                }
             } else if (userInput[0] == "exit") {
                 leaveMachine = true
             } else {
                 println "Unrecognized command!"
             }
         }
+
+        println '''
+Thank you for shopping RoboBroVend!
+
+We know you have a choice of vending options, and we at RoboBroVend
+would like to say thank you for supporting our all-natural, free-range,
+locally-sourced, organic, non-GMO products.
+'''
     }
 }
--- src/main/VendingMachine.groovy
+++ src/main/VendingMachine.groovy
@@ -9,9 +9,17 @@ class VendingMachine {
     float balance = 0.00
 
     Map<String,Float> coins = [
-        "nickel": 0.05,
-        "dime": 0.10,
-        "quarter": 0.25
+            "nickel": 0.05,
+            "dime": 0.10,
+            "quarter": 0.25
+    ]
+
+    List<String> coinReturn = []
+
+    Map<String,Float> menu = [
+            "soda": 1.0,
+            "chips": 0.5,
+            "candy": 0.65
     ]
 
 
@@ -26,11 +34,35 @@ class VendingMachine {
         return "INSERT COIN"
     }
 
+    List<String> displayMenu() {
+        List<String> output = []
+        menu.each { String name, Float price ->
+            output.add(String.format("%s: \$%.2f", name, price))
+        }
+        return output
+    }
+
     String insertCoin(String coin) {
         if (coins.containsKey(coin.toLowerCase())) {
             balance += coins[coin.toLowerCase()]
+        } else {
+            coinReturn.add(coin)
         }
         displayBalance()
+    }
+
+    String selectProduct(String product) {
+        if (menu.containsKey(product.toLowerCase())) {
+            Float price = menu[product.toLowerCase()]
+            if (balance >= price) {
+                balance -= price
+                return "THANK YOU"
+            } else {
+                return String.format("Price: \$%.2f, balance: \$%.2f", price, balance)
+            }
+        } else {
+            return "UNKNOWN PRODUCT"
+        }
     }
 
 }
